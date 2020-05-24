54f7dfe399d659d5d35373e4ba3a7fa12783aff1
Ihor Pankiv <harry.pankiv@gmail.com>
2020-05-24 19:39:29 +0300

    feat: added webpack setup Allowed to perform scan on table 'project'

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: slsw.lib.entries,
+  target: 'node',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+    sourceMapFilename: '[file].map'
+  }
+};
